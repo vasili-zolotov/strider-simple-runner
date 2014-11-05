5e87f89801fce2fbac0df0dccc38b6a256d4155e
Vasili Zolotov <vasili.zolotov@paperg.com>
2014-11-05 13:29:18 -0800

    Changing hint pattern. To set hint to "hint text" we do this...
    OK strider: hint text!

--- test/test_hints.js
+++ test/test_hints.js
@@ -4,20 +4,26 @@ var hints = require('../lib/hints');
 describe('hints', function () {
   describe('.parse', function () {
     it('should match the first hint in message', function () {
-      var message = 'making some changes @strider  test this   @strider ignore this';
+      var message = 'making some changes. OK strider: test this ! OK strider: ignore this.';
       expect(hints.parse(message)).to.eql('test this');
     });
-    
+
+    it('should handle newline', function () {
+      var message = 'making some changes\nok strider: test this!\n\nsome more text';
+      expect(hints.parse(message)).to.eql('test this');
+    });
+
     it('should return undefined with no hints in message', function () {
       var message = 'making some changes';
       expect(hints.parse(message)).to.eql(undefined);
     });
     
-    it('should handle newline', function () {
-      var message = 'making some changes\n@strider test this\n\nsome more text';
-      expect(hints.parse(message)).to.eql('test this');
+
+    it('should require an exclamation', function () {
+      var message = 'making some changes\nok strider: test this';
+      expect(hints.parse(message)).to.eql(undefined);
     });
-    
+
     it('should return undefined when input is undefined', function () {
       expect(hints.parse(undefined)).to.eql(undefined);
     });
