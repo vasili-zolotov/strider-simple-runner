d27db38717beb97a76be51c6ba1f3590815ab4d8
Vasili Zolotov <vasili.zolotov@paperg.com>
2014-11-05 09:04:57 -0800

    add @strider commit message hints

---
+++ test/test_hints.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var hints = require('../lib/hints');
+  
+describe('hints', function () {
+  describe('.parse', function () {
+    it('should match the first hint in message', function () {
+      var message = 'making some changes @strider  test this   @strider ignore this';
+      expect(hints.parse(message)).to.eql('test this');
+    });
+    
+    it('should return undefined with no hints in message', function () {
+      var message = 'making some changes';
+      expect(hints.parse(message)).to.eql(undefined);
+    });
+    
+    it('should handle newline', function () {
+      var message = 'making some changes\n@strider test this\n\nsome more text';
+      expect(hints.parse(message)).to.eql('test this');
+    });
+  });
+});
